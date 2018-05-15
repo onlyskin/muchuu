e6ef1ae66345453f3e928140c24c823af32ea9f7
Sam Szreter <s.szreter@gmail.com>
2018-05-15 14:04:46 +0300

    Add login route, and secret route which redirects to login when not authorized

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
+from flask import Flask, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET'])
+@app.route('/')
 def index():
     return 'Hello World'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'GET':
+        return '<form></form>'
+
+    return ''
+
+@app.route('/user_details')
+def user_details():
+    return redirect(login)
--- test_app.py
+++ test_app.py
@@ -2,10 +2,35 @@ import pytest
 
 from app import app
 
-def test_serves_index():
-    client = app.test_client()
+@pytest.fixture
+def client():
+    yield app.test_client()
 
+def test_serves_index(client):
     response = client.get('/')
 
     assert response.status_code == 200
     assert 'Hello World' in response.data
+
+def test_servers_login_page(client):
+    response = client.get('/login')
+
+    assert response.status_code == 200
+    assert '<form>' in response.data
+
+def test_can_post_to_login(client):
+    response = client.post('/login')
+
+    assert response.status_code == 200
+
+def test_accessing_restricted_page_redirects_to_login(client):
+    response = client.get('/user_details')
+
+    assert response.status_code == 302
+
+def xtest_can_read_user_details_when_logged_in(client):
+    response = client.post('/login')
+    response = client.get('/user_details')
+
+    assert response.status_code == 200
+    assert 'Secret details' in response.data
