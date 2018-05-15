1c1c77209b993c8a9e1036d8edc8606b3bd90add
Sam Szreter <s.szreter@gmail.com>
2018-05-15 17:09:34 +0300

    Group test cases

--- test_app.py
+++ test_app.py
@@ -7,48 +7,51 @@ app.testing = True
 def client():
     yield app.test_client()
 
-def test_serves_index(client):
-    response = client.get('/')
+class TestGetPages():
+     def test_serves_index(self, client):
+         response = client.get('/')
+     
+         assert response.status_code == 200
+         assert 'Hello World' in response.data
+     
+     def test_serves_login_page(self, client):
+         response = client.get('/login')
+     
+         assert response.status_code == 200
+         assert '<form action="/login" method="post">' in response.data
 
-    assert response.status_code == 200
-    assert 'Hello World' in response.data
+class TestLoggingIn():
+    def test_successful_login_redirects_to_user_details(self, client):
+        payload = dict(
+            password='good_password',
+        )
+        response = client.post('/login', data=payload)
+    
+        assert response.status_code == 302
+        assert response.location.endswith('/user_details')
+    
+    def test_unsuccessful_login_redirects_to_login(self, client):
+        payload = dict(
+            password='bad_password',
+        )
+        response = client.post('/login', data=payload)
+    
+        assert response.status_code == 302
+        assert response.location.endswith('/login')
 
-def test_serves_login_page(client):
-    response = client.get('/login')
-
-    assert response.status_code == 200
-    assert '<form action="/login" method="post">' in response.data
-
-def test_successful_login_redirects_to_user_details(client):
-    payload = dict(
-        password='good_password',
-    )
-    response = client.post('/login', data=payload)
-
-    assert response.status_code == 302
-    assert response.location.endswith('/user_details')
-
-def test_unsuccessful_login_redirects_to_login(client):
-    payload = dict(
-        password='bad_password',
-    )
-    response = client.post('/login', data=payload)
-
-    assert response.status_code == 302
-    assert response.location.endswith('/login')
-
-def test_cannot_access_details_page_when_not_logged_in(client):
-    response = client.get('/user_details')
-
-    assert response.status_code == 302
-
-def test_can_access_details_page_when_logged_in(client):
-    login(client)
-
-    response = client.get('/user_details')
-
-    assert response.status_code == 200
-    assert 'Secret details' in response.data
+class TestUserDetailsAuthentication():
+    def test_cannot_access_details_page_when_not_logged_in(self, client):
+        response = client.get('/user_details')
+    
+        assert response.status_code == 302
+    
+    def test_can_access_details_page_when_logged_in(self, client):
+        login(client)
+    
+        response = client.get('/user_details')
+    
+        assert response.status_code == 200
+        assert 'Secret details' in response.data
 
 def login(client):
     payload = dict(
